3cf875580645bfa33cadcb6502749cf550ec13c1
t-alam11 <mohammed.alam.2@city.ac.uk>
2025-05-21 15:02:51 +0100

    Guessing gone right

--- Guessing.py
+++ Guessing.py
@@ -1,1 +1,17 @@
-test
+import random
+
+def guessing(guess):
+    answer=0
+    computer=random.randint(1,guess)
+    while computer!=answer:
+        answer=int(input(f"Guess the number between 1 and {guess}: "))
+        if answer>computer:
+            print ("Too high")
+        elif answer<computer:
+            print("Too low")
+
+    print(f"You guessed right!, it was {answer}")
+
+
+guessing(10)
+
