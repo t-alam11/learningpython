c69447808ab326ec503cada74248f7fb3a52bfb4
t-alam11 <mohammed.alam.2@city.ac.uk>
2025-05-21 14:42:36 +0100

    Calculator

--- Main.py
+++ Main.py
@@ -1,3 +1,21 @@
 #Calculator
-number1=float(input("Provide the first number"))
-operate=input("Enter the operator you want to use"):
+number1=float(input("Provide the first number: "))
+
+while True:
+    operate = input("Enter the operator you want to use (+,-,*,/): ")
+    if operate in ["+","-","*","/"]:
+        break
+    else:
+        print("Invalid operator, provide correct one")
+
+number2=float(input("Provide the second number: "))
+
+if operate == "+":
+    print(number1+number2)
+elif operate == "-":
+    print(number1-number2)
+elif operate == "*":
+    print(number1*number2)
+elif operate == "/":
+    print(number1/number2)
+
